ec911e9b77cae823fe202b3f6e40112fad423196
Shae Smith <sbsmith86@gmail.com>
2019-09-23 18:10:26 -0400

    updates to layout

--- docs/dist/bundle.js
+++ docs/dist/bundle.js
@@ -26021,34 +26021,48 @@ function ReactSlidingPane(_ref) {
       overlayClassName = _ref.overlayClassName,
       closeIcon = _ref.closeIcon,
       _ref$from = _ref.from,
-      from = _ref$from === void 0 ? 'right' : _ref$from,
+      from = _ref$from === void 0 ? "right" : _ref$from,
       width = _ref.width;
   var directionClass = "slide-pane_from_".concat(from);
   return react.createElement(Modal, {
-    className: "slide-pane ".concat(directionClass, " ").concat(className || ''),
+    className: "slide-pane ".concat(directionClass, " ").concat(className || ""),
     style: {
       content: {
-        width: width || '80%'
+        width: width || "80%"
       }
     },
-    overlayClassName: "slide-pane__overlay ".concat(overlayClassName || ''),
+    overlayClassName: "slide-pane__overlay ".concat(overlayClassName || ""),
     closeTimeoutMS: CLOSE_TIMEOUT,
     isOpen: isOpen,
     onAfterOpen: onAfterOpen,
     onRequestClose: onRequestClose,
-    contentLabel: "Modal \"".concat(title || '', "\"")
+    contentLabel: 'Modal "'.concat(title || "", '"')
   }, react.createElement("div", {
     className: "slide-pane__header"
   }, react.createElement("div", {
     className: "slide-pane__close",
     onClick: onRequestClose
-  }, closeIcon ? closeIcon : react.createElement(IconClose, null)), react.createElement("div", {
-    className: "slide-pane__title-wrapper"
-  }, react.createElement("h2", {
-    className: "slide-pane__title"
-  }, title), react.createElement("div", {
-    className: "slide-pane__subtitle"
-  }, subtitle))), react.createElement("div", {
+  }, closeIcon ? closeIcon : react.createElement(IconClose, null)) // React.createElement(
+  //   "div",
+  //   {
+  //     className: "slide-pane__title-wrapper"
+  //   },
+  //   React.createElement(
+  //     "h2",
+  //     {
+  //       className: "slide-pane__title"
+  //     },
+  //     title
+  //   ),
+  //   React.createElement(
+  //     "div",
+  //     {
+  //       className: "slide-pane__subtitle"
+  //     },
+  //     subtitle
+  //   )
+  // )
+  ), react.createElement("div", {
     className: "slide-pane__content"
   }, children));
 }
@@ -26062,7 +26076,7 @@ ReactSlidingPane.propTypes = {
   children: propTypes.any.isRequired,
   className: propTypes.string,
   overlayClassName: propTypes.string,
-  from: propTypes.oneOf(['left', 'right', 'bottom']),
+  from: propTypes.oneOf(["left", "right", "bottom"]),
   width: propTypes.string,
   closeIcon: propTypes.any
 };
@@ -26105,7 +26119,7 @@ function styleInject(css, ref) {
   }
 }
 
-var css = ".slide-pane {\n  display: flex;\n  flex-direction: column;\n  background: #fff;\n  min-width: 100px;\n  height: 100%;\n  box-shadow: 0 8px 8px rgba(0,0,0,0.5);\n  transition: transform 0.5s;\n  will-change: transform;\n}\n.slide-pane:focus {\n  outline-style: none;\n}\n.slide-pane_from_right {\n  margin-left: auto;\n  transform: translateX(100%);\n}\n.slide-pane_from_right.ReactModal__Content--after-open {\n  transform: translateX(0%);\n}\n.slide-pane_from_right.ReactModal__Content--before-close {\n  transform: translateX(100%);\n}\n.slide-pane_from_left {\n  margin-right: auto;\n  transform: translateX(-100%);\n}\n.slide-pane_from_left.ReactModal__Content--after-open {\n  transform: translateX(0%);\n}\n.slide-pane_from_left.ReactModal__Content--before-close {\n  transform: translateX(-100%);\n}\n.slide-pane_from_bottom {\n  height: 90vh;\n  margin-top: 10vh;\n  transform: translateY(100%);\n}\n.slide-pane_from_bottom.ReactModal__Content--after-open {\n  transform: translateY(0%);\n}\n.slide-pane_from_bottom.ReactModal__Content--before-close {\n  transform: translateY(100%);\n}\n.slide-pane__overlay {\n  position: fixed;\n  top: 0px;\n  left: 0px;\n  right: 0px;\n  bottom: 0px;\n  background-color: rgba(0,0,0,0);\n}\n.slide-pane__overlay.ReactModal__Overlay--after-open {\n  background-color: rgba(0,0,0,0.3);\n  transition: background-color 0.5s;\n}\n.slide-pane__overlay.ReactModal__Overlay--before-close {\n  background-color: rgba(0,0,0,0);\n}\n.slide-pane__header {\n  display: flex;\n  flex: 0 0 64px;\n  align-items: center;\n  background: #ebebeb;\n  height: 64px;\n  border-bottom: 1px solid rgba(0,0,0,0.1);\n}\n.slide-pane__title-wrapper {\n  display: flex;\n  flex: 1;\n  flex-direction: column;\n  margin-left: 32px;\n  min-width: 0;\n}\n.slide-pane .slide-pane__title {\n  font-size: 18px;\n  font-weight: normal;\n  max-width: 80%;\n  white-space: nowrap;\n  overflow: hidden;\n  text-overflow: ellipsis;\n  margin: 0;\n  padding: 0;\n}\n.slide-pane__close {\n  margin-left: 24px;\n  padding: 16px;\n  opacity: 0.7;\n  cursor: pointer;\n}\n.slide-pane__close svg {\n  width: 12px;\n  padding: 0;\n}\n.slide-pane__content {\n  position: relative;\n  overflow-y: visible;\n  padding: 24px 32px;\n  flex: 1 1 auto;\n}\n.slide-pane__subtitle {\n  font-size: 12px;\n  margin-top: 2px;\n}\n";
+var css = ".slide-pane {\n  display: flex;\n  flex-direction: column;\n  background: #fff;\n  min-width: 100px;\n  height: 100%;\n  box-shadow: 0 8px 8px rgba(0, 0, 0, 0.5);\n  transition: transform 0.5s;\n  will-change: transform;\n}\n.slide-pane:focus {\n  outline-style: none;\n}\n.slide-pane_from_right {\n  margin-left: auto;\n  transform: translateX(100%);\n}\n.slide-pane_from_right.ReactModal__Content--after-open {\n  transform: translateX(0%);\n}\n.slide-pane_from_right.ReactModal__Content--before-close {\n  transform: translateX(100%);\n}\n.slide-pane_from_left {\n  margin-right: auto;\n  transform: translateX(-100%);\n}\n.slide-pane_from_left.ReactModal__Content--after-open {\n  transform: translateX(0%);\n}\n.slide-pane_from_left.ReactModal__Content--before-close {\n  transform: translateX(-100%);\n}\n.slide-pane_from_bottom {\n  height: 90vh;\n  margin-top: 10vh;\n  transform: translateY(100%);\n}\n.slide-pane_from_bottom.ReactModal__Content--after-open {\n  transform: translateY(0%);\n}\n.slide-pane_from_bottom.ReactModal__Content--before-close {\n  transform: translateY(100%);\n}\n.slide-pane__overlay {\n  position: fixed;\n  top: 0px;\n  left: 0px;\n  right: 0px;\n  bottom: 0px;\n  background-color: rgba(0, 0, 0, 0);\n}\n.slide-pane__overlay.ReactModal__Overlay--after-open {\n  background-color: rgba(0, 0, 0, 0.3);\n  transition: background-color 0.5s;\n}\n.slide-pane__overlay.ReactModal__Overlay--before-close {\n  background-color: rgba(0, 0, 0, 0);\n}\n.slide-pane__header {\n  display: flex;\n  flex: 0 0 64px;\n  justify-content: flex-end;\n  align-items: center;\n  height: 64px;\n  border-bottom: 1px solid rgba(0, 0, 0, 0.1);\n}\n.slide-pane__title-wrapper {\n  display: flex;\n  flex: 1;\n  flex-direction: column;\n  margin-left: 32px;\n  min-width: 0;\n}\n.slide-pane .slide-pane__title {\n  font-size: 18px;\n  font-weight: normal;\n  max-width: 80%;\n  white-space: nowrap;\n  overflow: hidden;\n  text-overflow: ellipsis;\n  margin: 0;\n  padding: 0;\n}\n.slide-pane__close {\n  margin-left: 24px;\n  padding: 16px;\n  opacity: 0.7;\n  cursor: pointer;\n}\n.slide-pane__close svg {\n  width: 12px;\n  padding: 0;\n}\n.slide-pane__content {\n  position: relative;\n  overflow-y: visible;\n  padding: 24px 32px;\n  flex: 1 1 auto;\n}\n.slide-pane__subtitle {\n  font-size: 12px;\n  margin-top: 2px;\n}\n";
 styleInject(css);
 
 var App =
