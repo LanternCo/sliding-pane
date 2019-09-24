cb549c3dfbbf8ce069b992a4c879f7a8874c9026
Shae Smith <sbsmith86@gmail.com>
2019-09-24 09:08:14 -0400

    update overflow

--- docs/dist/bundle.js
+++ docs/dist/bundle.js
@@ -26013,8 +26013,8 @@ var CLOSE_TIMEOUT = 500;
 function ReactSlidingPane(_ref) {
   var isOpen = _ref.isOpen,
       title = _ref.title,
-      subtitle = _ref.subtitle,
-      onRequestClose = _ref.onRequestClose,
+      // subtitle = _ref.subtitle,
+  onRequestClose = _ref.onRequestClose,
       onAfterOpen = _ref.onAfterOpen,
       children = _ref.children,
       className = _ref.className,
